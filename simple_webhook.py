5634bb5d5f5442f5258d71a84bc24dd04ee2e13c
jkbczm <123495788+jkbczm@users.noreply.github.com>
2025-04-24 10:11:27 +0200

    Create simple_webhook.py
    write and comment the code

---
+++ simple_webhook.py
@@ -0,0 +1,11 @@
+# importing libraries needed for the webhook
+import requests
+#initiating your webhook url
+webhook_url='yourwebhookurl'
+# initiating the data you want to send
+data = {
+'event':'user_signed_up',
+'user_id':12345
+}
+#sending the data to your webhook
+requests.post(webhook_url, json=data)
