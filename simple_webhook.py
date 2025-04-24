60b982f5e48e9a19d44217231e63257069dd6d0c
jkbczm <123495788+jkbczm@users.noreply.github.com>
2025-04-24 17:39:46 +0200

    add message to simple_webhook.py

--- simple_webhook.py
+++ simple_webhook.py
@@ -2,10 +2,13 @@
 import requests
 #initiating your webhook url
 webhook_url='yourwebhookurl'
+#message you want to send 
+message ='yourmessage'
 # initiating the data you want to send
 data = {
 'event':'user_signed_up',
-'user_id':12345
+'user_id':12345,
+'content':'message'
 }
 #sending the data to your webhook
 requests.post(webhook_url, json=data)
