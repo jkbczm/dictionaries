cd9c6baeb7d5eb4defd9a562fd6f4c5477f204de
jkbczm <jakub.czmochowski@gmail.com>
2025-04-26 19:25:21 +0200

    update better webhook

--- better_webhook.py
+++ better_webhook.py
@@ -1,14 +1,14 @@
 # importing libraries needed for the webhook
 import requests
 #initiating your webhook url
-webhook_url='yourwebhookurl'
+webhook_url='https://discord.com/api/webhooks/1365738814555033610/XsbRbh2vBVB0ke_HyayJsZG4SVHS6HPQQ4deAuivE0SzF8zXZeE2RA1E7YoyKWgL88iQ'
 #message you want to send 
-text='yourmessage'
+text= str(input("your message"))
 # initiating the data you want to send
 data = {
 'event':'user_signed_up',
 'user_id':12345,
-'content':'text'
+'content': text
 }
 #making a function for sending information
 def send(message,url):
